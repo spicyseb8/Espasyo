bf731378e714a1a031f20cedb277619ad38c5ab0
spicyseb8 <sebbqtqt@gmail.com>
2026-07-12 16:34:58 +0800

    floor rendering complete

--- client/src/scene/Floors/Floor.tsx
+++ client/src/scene/Floors/Floor.tsx
@@ -1,63 +1,70 @@
-import { Shape, ShapeGeometry, DoubleSide, Vector3 } from "three";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
+import { useTexture } from "@react-three/drei";
+import {
+    Shape,
+    ShapeGeometry,
+    DoubleSide,
+    Vector3,
+    RepeatWrapping,
+} from "three";
 
 interface FloorProps {
     polygon: Vector3[];
 }
 
-export default function Floor({
-    polygon
-}: FloorProps) {
+export default function Floor({ polygon }: FloorProps) {
+    // Load PBR textures
+    const textures = useTexture({
+        map: "/textures/floor/wood-color.png",
+        normalMap: "/textures/floor/wood-normal.png",
+        roughnessMap: "/textures/floor/wood-roughness.png",
+    });
 
+    // Configure texture tiling
+    useEffect(() => {
+        Object.values(textures).forEach((texture) => {
+            texture.wrapS = RepeatWrapping;
+            texture.wrapT = RepeatWrapping;
+            texture.repeat.set(0.5, 0.5);
+            texture.needsUpdate = true;
+        });
+    }, [textures]);
+
+    // Generate floor geometry from the room polygon
     const geometry = useMemo(() => {
-
-        if (!polygon)
-            return null;
+        if (!polygon) return null;
 
         const valid = polygon.filter(Boolean);
 
-        if (valid.length < 3)
-            return null;
+        if (valid.length < 3) return null;
 
-        console.log("Polygon");
-        valid.forEach((p, i) => {
-            console.log(i, p.x, p.y, p.z);
-        });
+        const shape = new Shape();
 
-  const shape = new Shape();
+        shape.moveTo(valid[0].x, -valid[0].z);
 
-shape.moveTo(valid[0].x, -valid[0].z);
+        for (let i = 1; i < valid.length; i++) {
+            shape.lineTo(valid[i].x, -valid[i].z);
+        }
 
-for (let i = 1; i < valid.length; i++) {
-    shape.lineTo(valid[i].x, -valid[i].z);
-}
+        shape.closePath();
 
-shape.closePath();
-
-return new ShapeGeometry(shape);
-
+        return new ShapeGeometry(shape);
     }, [polygon]);
 
-    if (!geometry)
-        return null;
+    if (!geometry) return null;
 
     return (
-
         <mesh
             geometry={geometry}
             rotation={[-Math.PI / 2, 0, 0]}
-            onUpdate={(mesh) => {
-                console.log("Floor mesh position:", mesh.position);
-            }}
         >
-
             <meshStandardMaterial
-                color="#f7af2a"
+                map={textures.map}
+                normalMap={textures.normalMap}
+                roughnessMap={textures.roughnessMap}
+                metalness={0}
                 side={DoubleSide}
             />
-
         </mesh>
-
     );
-
 }
--- client/src/scene/Grid.tsx
+++ client/src/scene/Grid.tsx
@@ -2,14 +2,15 @@ import { Grid } from "@react-three/drei";
 
 export default function SceneGrid() {
   return (
-    <Grid
-      args={[100, 100]}
-      cellSize={1}
-      cellThickness={1}
-      sectionSize={4}
-      sectionThickness={1}
-      infiniteGrid
-      fadeDistance={50}
-    />
+   <Grid
+    position={[0, -0.01, 0]}
+    args={[60, 60]}
+    cellSize={1}
+    cellThickness={1}
+    sectionSize={4}
+    sectionThickness={1}
+    infiniteGrid = {false}
+    fadeDistance={40}
+/>
   );
 }
